2f55c8f620ed2babf8754dd5c28060bb64e22896
Clara <75179363+quietbrelax@users.noreply.github.com>
2025-11-11 12:22:34 +0100

    Add files via upload

---
+++ grid.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include "raylib.h"
+
+class Grid{
+
+    public:
+        Grid();
+        void Initialize();
+        int grid[20][10];
+        void Draw();
+        bool IsCellOutside(int row, int column);
+        void print();
+        bool IsCellEmpty(int row, int column);
+        int ClearFullRows();
+
+    private:
+        bool IsRowFull(int row);
+        void ClearRow(int row);
+        void MoveRowDown(int row, int numRows);
+        int numRows;
+        int numCols;
+        int cellSize;
+        std::vector<Color> colors;
+};
